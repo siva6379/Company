db622abcfcb52112967905f3705a63b97ece6116
siva6379 <sivaarumugam0910@gmail.com>
2021-12-09 18:02:26 +0530

    Task completed

---
+++ src/org/company/Employee.java
@@ -0,0 +1,35 @@
+package org.company;
+
+public class Employee {
+	public static void main(String[] args) {
+		byte empAge = 25;
+		System.out.println(empAge);
+		System.out.println("empAge"+1);
+		System.out.println(empAge+1);
+		short empId = 1234;
+		System.out.println("employee id is "+empId);
+		int empPincode = 600042;
+		System.out.println("employee pincode is"+empPincode);
+		long empPhoneNo = 6379405306l;
+		System.out.println("employee phoneno is"+ empPhoneNo);
+		float empSalary = 5678.987654f;
+		System.out.println("employee salary is "+empSalary);
+		float x = 20/3f;
+		System.out.println(x);
+		double empAnswer = 23.453215657;
+		System.out.println("employee answer is "+empAnswer);
+		double y = 20/3d;
+		System.out.println(y);
+		char empGender = 'M';
+		System.out.println("employee gender is "+empGender);
+		String empGmail = "sivaarumugam0910@gmail.com";
+		System.out.println("employee gmail is "+ empGmail);
+		boolean empStatus = true;
+		System.out.println("employee maritial status is"+empStatus);
+		
+		
+		
+		
+	}
+
+}
